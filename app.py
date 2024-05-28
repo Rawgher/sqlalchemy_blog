b8f9275546d2259371aaad704e1b93cb49729f24
Rawgher <rdpender3@gmail.com>
2024-05-28 13:50:27 -0700

    add user form working

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from models import db, connect_db, User
 from flask_debugtoolbar import DebugToolbarExtension
 from dotenv import load_dotenv
@@ -25,7 +25,28 @@ def home():
 
 @app.route("/users")
 def all_users():
-    """ List all users in the database"""
+    """List all users in the database"""
 
     users = User.query.all()
     return render_template("user-list.html", users=users)
+
+@app.route("/users/new")
+def new_user():
+    """Show new user form"""
+    return render_template("new-user.html")
+
+@app.route("/users/new", methods=["POST"])
+def add_user():
+    """Add a new user to the database"""
+
+    first = request.form['first']
+    last = request.form['last']
+    pic_url = request.form['pic-url']
+
+    user= User(first_name=first, last_name=last, image_url=pic_url)
+
+    db.session.add(user)
+    db.session.commit()
+
+    users = User.query.all()
+    return redirect('/users')
