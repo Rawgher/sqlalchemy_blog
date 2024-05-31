f6e82979b144122fbe0e230902f3d2915d30f201
Rawgher <rdpender3@gmail.com>
2024-05-31 09:33:43 -0700

    able to view and add tags

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
 from flask import Flask, redirect, render_template, request
-from models import db, connect_db, User, Post
+from models import db, connect_db, User, Post, Tag, PostTag
 from flask_debugtoolbar import DebugToolbarExtension
 from dotenv import load_dotenv
 import os
@@ -31,45 +31,64 @@ def all_users():
     """List all users in the database"""
 
     users = User.query.all()
-    return render_template("user-list.html", users=users)
+    return render_template("user/user-list.html", users=users)
 
 @app.route("/users/new")
 def new_user():
     """Show new user form"""
-    return render_template("new-user.html")
+    return render_template("user/new-user.html")
 
 @app.route("/users/<int:user_id>")
 def single_user(user_id):
     """Show a single user account"""
 
     user = User.query.get_or_404(user_id)
-    return render_template("user.html", user=user)
+    return render_template("user/user.html", user=user)
 
 @app.route("/users/<int:user_id>/edit")
 def show_edit_user(user_id):
     """Show a single user account"""
 
     user = User.query.get_or_404(user_id)
-    return render_template("edit-user.html", user=user)
+    return render_template("user/edit-user.html", user=user)
 
 @app.route("/users/<int:user_id>/posts/new")
 def new_post_form(user_id):
     """Show new post form"""
     user = User.query.get_or_404(user_id)
-    return render_template("new-post.html")
+    return render_template("posts/new-post.html")
 
 @app.route("/posts/<int:post_id>")
 def show_post(post_id):
     """Show individual post"""
     post = Post.query.get_or_404(post_id)
-    return render_template("post.html", post=post)
+    return render_template("posts/post.html", post=post)
 
 @app.route("/posts/<int:post_id>/edit")
 def show_edit_post(post_id):
     """Show a single user account"""
 
     post = Post.query.get_or_404(post_id)
-    return render_template("edit-post.html", post=post)
+    return render_template("posts/edit-post.html", post=post)
+
+@app.route("/tags")
+def all_tags():
+    """List all tags in the database"""
+
+    tags = Tag.query.all()
+    return render_template("tags/tags-list.html", tags=tags)
+
+@app.route("/tags/<int:tag_id>")
+def current_tag(tag_id):
+    """Show single tag and all its associated posts from the database"""
+
+    tag = Tag.query.get_or_404(tag_id)
+    return render_template("tags/tag.html", tag=tag)
+
+@app.route("/tags/new")
+def new_tag_form():
+    """Show new tag form"""
+    return render_template("tags/new-tag.html")
 
 # POST ROUTES -------------------------------------------------
 
@@ -126,6 +145,18 @@ def edit_post(post_id):
     db.session.commit()
 
     return redirect(f'/posts/{post_id}')
+
+@app.route("/tags/new", methods=["POST"])
+def add_tag():
+    """Add a new tag to the database"""
+
+    name = request.form['name']
+
+    tag = Tag(name=name)
+
+    db.session.add(tag)
+    db.session.commit()
+    return redirect('/tags')
 
 # DELETE ROUTES -----------------------------------------------
 
